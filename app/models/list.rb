416f14bab010175528fe6452c769c0bfc9ceab9c
Simone Piccolo <simonepiccolo1987@gmail.com>
2022-04-30 13:25:19 +0100

    n-n associaton done, validations setted, next step is create routes controller and view

---
+++ app/models/list.rb
@@ -0,0 +1,7 @@
+class List < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks, dependent: :destroy
+  validates :name, uniqueness: { scope: :name, message: "already in list"}
+  validates :name, presence: true
+
+end
